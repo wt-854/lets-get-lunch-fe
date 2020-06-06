50af19cde6eea06ba31bbb10c6970f1a3d7ba197
wt-854 <wt854@uowmail.edu.au>
2020-06-07 00:21:52 +0800

    Add user signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'signup',
+    loadChildren: () => import('./signup/signup.module')
+      .then(m => m.SignupModule)
+  }
 ];
 
 @NgModule({
