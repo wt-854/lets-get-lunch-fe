170269fa27ac2035fc046202d5ec30f3634b3e38
wt854 <wt854@outlook.com>
2020-06-13 16:57:52 +0800

    Add calendar to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { EventsService } from '../services/events/events.service';
+import { Event } from '../services/events/event';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  view = 'week';
+  viewDate: Date = new Date();
+  events: Array<Event>;
+  error: string;
+  noEvents: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private eventsService: EventsService
+  ) { }
 
   ngOnInit() {
+    const id = this.authService.currentUser()._id;
+    this.eventsService.getUserEvents(id).subscribe(res => {
+      console.log('events for user ', res);
+      if (res) {
+        this.events = this.addJSDate(res);
+        this.events = this.addEventColors(this.events);
+      } else {
+        this.noEvents = 'You are not a member of any events.';
+      }
+    }, err => {
+      this.error = err.error.message;
+    });
+  }
+
+  addJSDate(events: Array<Event>): Array<Event> {
+    return events.map((event) => {
+      event.start = new Date(event.startTime);
+      event.end = new Date(event.endTime);
+      return event;
+    });
+  }
+
+  addEventColors(events: Array<Event>): Array<Event> {
+    return events.map((event) => {
+      event.color = { primary: '#1E90FF', secondary: '#D1E8FF' };
+      return event;
+    });
+  }
+
+  eventClicked(event) {
+    // Route to event view page
   }
 
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'angular-calendar';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -9,7 +10,8 @@ import { DashboardComponent } from './dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    CalendarModule.forRoot()
   ]
 })
 export class DashboardModule { }
