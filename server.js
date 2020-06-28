dafa10ea326a774ed1ad682ed1c4cfe3a552519f
wt854 <wt854@outlook.com>
2020-06-29 04:12:43 +0800

    idk

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
+// server.js
 const express = require('express');
 const path = require('path');
-let app = express();  // const app
-var cors = require('cors');
+let cors = require('cors');
+let app = express();
 
-app.use(cors()); 
-
-// If an incoming request uses a protocol other than HTTPS, 
-// redirect that request to the same url but with HTTPS
-const forceSSL = function () {
+app.use(cors());
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
   return function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
+       ['https://', req.get('Host'), req.url].join('')
       );
     }
     next();
   }
 }
-// Instruct the app to use forceSSL middleware
+// Instruct the app
+// to use the forceSSL
+// middleware
 app.use(forceSSL());
-// Run the app by serving the static files in the dist directory
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist/lets-get-lunch'));
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/lets-get-lunch/index.html'));
 });
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
+console.log(`Running server.js`);
 app.listen(process.env.PORT || 8080);
