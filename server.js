af0541ffa801d170569a4a27b6ba0b589e947cfa
wt854 <wt854@outlook.com>
2020-06-29 01:56:20 +0800

    access control allow origin 2

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 const forceSSL = function () {
   return function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
         ['https://', req.get('Host'), req.url].join('')
