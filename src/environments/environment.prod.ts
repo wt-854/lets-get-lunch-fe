efcaa7efb6b6c10a059d3a5a1b36f26179b8ed3e
wt854 <wt854@outlook.com>
2020-06-29 00:55:57 +0800

    CORRECT api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMaps: 'AIzaSyCmI2yCjeaVkDUeyYH5AjkbAzpnlNvW6aQ',
-  api: 'https://lets-get-lunch-api-wt854.herokuapp.com'
+  api: 'https://lets-get-lunch-api-wt854.herokuapp.com/api'
 };
